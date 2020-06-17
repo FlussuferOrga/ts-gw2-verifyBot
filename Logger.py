2562b7bd30385b9c822a1fed08bb3d595b6e420c
Tobias Günther <Xyaren@users.noreply.github.com>
2020-06-18 01:12:43 +0200

    reformatted in PEP 8 style cleanup added .editorconfig fixed .dockerignore and .gitignore for venv optimized imports

--- Logger.py
+++ Logger.py
@@ -1,9 +1,9 @@
-import logging
 import inspect
+import logging
 
 
-def getLogger(name = None, file = "ts3bot.log", level = logging.DEBUG, generate_name = True):
-    '''
+def getLogger(name=None, file="ts3bot.log", level=logging.DEBUG, generate_name=True):
+    """
     Retrieves a specific logger with fixed configuration.
     The philosophy behind logging is explained in https://docs.python.org/3/howto/logging.html
     To avoid a bit of boiler place code and setup, this method
@@ -13,19 +13,19 @@ def getLogger(name = None, file = "ts3bot.log", level = logging.DEBUG, generate_
     and also to set the level parameter from a config file; see parameter
     documentation for details.
 
-    name: name of the logger. This can be omitted and will default to the name 
+    name: name of the logger. This can be omitted and will default to the name
             of the calling module or "root", if called from the main module.
-    file: the file messages with DEBUG and above level are logged to. 
+    file: the file messages with DEBUG and above level are logged to.
             Defaults to ts3bot.log
     level: the global level for the logger. While two handlers exist for
-            printing to console and to file with their own level, 
+            printing to console and to file with their own level,
             the logger itself can be set to a certain log level (default: DEBUG)
             which can disable messages before reaching the respective handler,
             see https://docs.python.org/3/_images/logging_flow.png
-            Setting this to something above DEBUG can help when dealing with 
+            Setting this to something above DEBUG can help when dealing with
             excessive log output.
     generate_name: Automatically generates a name for the logger based on the calling module if name is not given.
-    '''
+    """
     formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
 
     if not name and generate_name:
@@ -40,13 +40,14 @@ def getLogger(name = None, file = "ts3bot.log", level = logging.DEBUG, generate_
     if not logger.hasHandlers():
         # initialize handlers
         logger.setLevel(level)
-        for h,l in ((logging.StreamHandler(), logging.WARNING), (logging.FileHandler(file), logging.DEBUG)):
+        for h, l in ((logging.StreamHandler(), logging.WARNING), (logging.FileHandler(file), logging.DEBUG)):
             h.setLevel(l)
             h.setFormatter(formatter)
-            logger.addHandler(h)  
+            logger.addHandler(h)
 
     return logger
 
+
 # set default logger to the log file
 # this also provides logging messages from other modules i.e. ts3.
-getLogger(generate_name=False) 
+getLogger(generate_name=False)
