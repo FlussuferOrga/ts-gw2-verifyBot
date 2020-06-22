f850159bef931f40f58c28ac6a2ab7ad92ce392e
Daniel O'Grady <danielogrady@gmx.de>
2020-06-22 23:26:44 +0200

    Add several more methods in the TS3Facade (tests pending)

--- TS3Facade.py
+++ TS3Facade.py
@@ -23,6 +23,43 @@ class TS3Facade:
 
         return Channel(resp["cid"], resp["channel_name"])
 
+    # FIXME: tests
+    def channel_info(self, channel_id: int):
+        resp, ts3qe = self._ts3_connection.ts3exec(lambda tsc: tsc.query("channelinfo", cid=channel_id).all())
+        return resp
+
+    # FIXME: tests
+    def channel_delete(self, channel_id: int, force: bool = False):
+        self._ts3_connection.ts3exec(lambda tsc: tsc.exec_("channeldelete", cid=channel_id, force=1 if force else 0))
+
+    # FIXME: tests
+    def servergroup_list(self):
+        resp, ts3qe = self._ts3_connection.ts3exec(lambda tsc: tsc.query("servergrouplist").all())
+        return resp
+
+    # FIXME: tests
+    def servergroup_delete(self, servergroup_id: int, force: bool = False):
+        self._ts3_connection.ts3exec(lambda tsc: tsc.exec_("servergroupdel", sgid=servergroup_id, force=1 if force else 0))
+
+    # FIXME: tests
+    def channel_create(self,
+                       channel_name: str,
+                       channel_description: str = "",
+                       channel_parent_id: int = 0,
+                       channel_flag_permanent: bool = True,
+                       channel_maxclients: int = -1,  # passing -1 makes the number of clients unlimited
+                       channel_order: int = 0
+                       ):
+        self._ts3_connection.ts3exec(lambda tsc: tsc.exec_("channelcreate",
+                                                           channel_name=channel_name,
+                                                           channel_description=channel_description,
+                                                           cpid=channel_parent_id,
+                                                           channel_flag_permanent=1 if channel_flag_permanent else 0,
+                                                           channel_maxclients=channel_maxclients,
+                                                           channel_order=channel_order,
+                                                           channel_flag_maxclients_unlimited=1 if channel_maxclients == -1 else 0).first(),
+                                     signal_exception_handler)
+
 
 class Channel:
     def __init__(self, channel_id, channel_name):
