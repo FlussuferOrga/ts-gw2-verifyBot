0212bcc508d5d009127c15fd41543922383eaf4d
Tobias Günther <Xyaren@users.noreply.github.com>
2020-08-29 15:01:35 +0200

    translate in-code documentation

--- bot/connection_pool/__init__.py
+++ bot/connection_pool/__init__.py
@@ -11,30 +11,30 @@ import time
 
 
 class TooManyConnections(Exception):
-    """当连接数过多时，抛出该异常"""
+    """When there are too many connections, throw this exception"""
 
 
 class Expired(Exception):
-    """当连接不可用时，抛出该异常"""
+    """When the connection is not available, throw this exception"""
 
 
 class UsageExceeded(Expired):
-    """连接的使用次数超出限制"""
+    """The number of uses of the connection exceeds the limit"""
 
 
 class TtlExceeded(Expired):
-    """连接使用时间超出 ttl 指定的生命周期"""
+    """The connection usage time exceeds the life cycle specified by ttl"""
 
 
 class IdleExceeded(Expired):
-    """闲置时间超出 idle 指定的时间"""
+    """Idle time exceeds the time specified by idle"""
 
 
 T = TypeVar('T')
 
 
 class WrapperConnection(ContextManager[T]):
-    """用于包装连接池中的数据库连接，以便处理生命周期逻辑"""
+    """Used to package database connections in the connection pool to handle life cycle logic"""
     connection: T
 
     def __init__(self, pool, connection: T):
@@ -44,13 +44,13 @@ class WrapperConnection(ContextManager[T]):
         self.last = self.created = time.time()
 
     def using(self):
-        """当连接被调用时使用该方法，使用次数 +1"""
+        """Use this method when the connection is called, the number of uses increases by 1"""
         self.usage += 1
         self.last = time.time()
         return self
 
     def reset(self):
-        """重置连接包装状态"""
+        """Reset connection package status"""
         self.usage = self.last = self.created = 0
 
     def __enter__(self) -> T:
@@ -61,15 +61,15 @@ class WrapperConnection(ContextManager[T]):
 
 
 class ConnectionPool(Generic[T]):
-    """连接池类，可用于 pymysql/memcache/redis/... 等
+    """Connection pool class, can be used for pymysql/memcache/redis/... 等
 
-    可通过如下方式调用：
+    It can be called as follows：
         pool = ConnectionPool(create=redis.Redis)
 
-    也可以通过 lambda 的方式指定 create 调用：
+    You can also specify the create call by lambda：
         pool = ConnectionPool(create=lambda: redis.Redis(host='127.0.0.1'))
 
-    或者通过 functools.partial
+    Or through functools.partial
         from functools import partial
         pool = ConnectionPool(create=partial(redis.Redis, host='127.0.0.1'))
     """
@@ -81,14 +81,14 @@ class ConnectionPool(Generic[T]):
                  max_size: int = 10, max_usage: int = 0,
                  ttl: int = 0, idle: int = 60,
                  block: bool = True) -> None:
-        """初始化参数
+        """Initialization parameters
 
-            create:    必须是一个可 callback 的函数
-            max_size:  最大连接数，当为 0 的时候则没有限制，不建议设置为 0
-            max_usage: 连接可使用次数，达到该次数后，连接将被释放/关闭
-            ttl:       连接可使用时间，单位(秒)，当连接使用达到指定时间后，连接将被释放/关闭
-            idle:      连接空闲时间，单位(秒)，当连接在闲置指定时间后，将被释放/关闭
-            block:     当连接数满的时候，是否阻塞等待连接被释放，输入 False 则在连接池满时会抛出异常
+            create: must be a callback function
+            max_size: The maximum number of connections. When it is 0, there is no limit. It is not recommended to set it to 0
+            max_usage: the number of times the connection can be used, after reaching this number, the connection will be released/closed
+            ttl: connection life time, unit (seconds), when the connection reaches the specified time, the connection will be released/closed
+            idle: connection idle time, unit (seconds), when the connection is idle for a specified time, it will be released/closed
+            block: When the number of connections is full, whether to block waiting for the connection to be released, input False to throw an exception when the connection pool is full
         """
         if not hasattr(create, '__call__'):
             raise ValueError('"create" argument is not callable')
@@ -105,12 +105,12 @@ class ConnectionPool(Generic[T]):
         self._size = 0
 
     def item(self) -> WrapperConnection[T]:
-        """可通过 with ... as ... 语法调用
+        """ can be called by with ... as ... syntax
 
-            pool = ConnectionPool(create=redis.Redis)
-            with pool.item() as redis:
-                redis.set('foo', 'bar)
-        """
+             pool = ConnectionPool(create=redis.Redis)
+             with pool.item() as redis:
+                 redis.set('foo','bar)
+         """
         self._lock.acquire()
 
         try:
@@ -118,12 +118,12 @@ class ConnectionPool(Generic[T]):
                 if not self._block:
                     raise TooManyConnections('Too many connections')
 
-                self._lock.wait()  # 等待闲置连接
+                self._lock.wait()  # Wait for idle connection
 
             try:
-                wrapped = self._pool.get_nowait()  # 从空闲连接池中获取一个
+                wrapped = self._pool.get_nowait()  # Get one from the free connection pool
             except queue.Empty:
-                wrapped = self._wrapper(self._create())  # 创建新连接
+                wrapped = self._wrapper(self._create())  # Create new connection
                 self._size += 1
         finally:
             self._lock.release()
@@ -131,9 +131,9 @@ class ConnectionPool(Generic[T]):
         return wrapped.using()
 
     def release(self, conn):
-        """释放一个连接，让连接重回到连接池中
+        """Release a connection, let the connection return to the connection pool
 
-        当连接使用超过限制/超过限定时间时，连接将被销毁
+         When the connection usage exceeds the limit / exceeds the limit time, the connection will be destroyed
         """
         self._lock.acquire()
         wrapped = self._wrapper(conn)
@@ -144,12 +144,12 @@ class ConnectionPool(Generic[T]):
             self._destroy(wrapped)
         else:
             self._pool.put_nowait(wrapped)
-            self._lock.notifyAll()  # 通知其它线程，有闲置连接可用
+            self._lock.notifyAll()  # Notify other threads that there are idle connections available
         finally:
             self._lock.release()
 
     def _destroy(self, wrapped):
-        """销毁一个连接"""
+        """Destroy a connection"""
         if self._destroy_function is not None:
             self._destroy_function(wrapped.connection)
 
@@ -157,7 +157,6 @@ class ConnectionPool(Generic[T]):
         self._size -= 1
 
     def _wrapper(self, conn: T) -> WrapperConnection[T]:
-        """利用 id 地址，对连接进行包装"""
         if isinstance(conn, WrapperConnection):
             return conn
 
@@ -169,7 +168,7 @@ class ConnectionPool(Generic[T]):
         return self.__wrappers[_id]
 
     def _unwrapper(self, wrapped):
-        """取消对连接的包装"""
+        """Unwrap the connection"""
         if not isinstance(wrapped, WrapperConnection):
             return
 
@@ -181,7 +180,7 @@ class ConnectionPool(Generic[T]):
             del self.__wrappers[_id]
 
     def _test(self, wrapped):
-        """测试连接的可用性，当不可用时，抛出 Expired 异常"""
+        """Test the availability of the connection, and throw an Expired exception when it is not available"""
         if self._max_usage and wrapped.usage >= self._max_usage:
             raise UsageExceeded('Usage exceeds %d times' % self._max_usage)
 
