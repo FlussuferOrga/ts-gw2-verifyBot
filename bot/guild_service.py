150cab61e4e72ff9a9fe646235c3e15d006e67a0
Tobias Günther <Xyaren@users.noreply.github.com>
2020-09-25 11:37:57 +0200

    extract emblem downloader

--- bot/guild_service.py
+++ bot/guild_service.py
@@ -1,17 +1,11 @@
 import logging
-from typing import Optional, Tuple
-
-import binascii
-import requests
 
 import bot.gwapi as gw2api
 from bot.config import Config
 from bot.connection_pool import ConnectionPool
 from bot.db import ThreadSafeDBConnection
+from bot.emblem_downloader import download_guild_emblem
 from bot.ts import TS3Facade, User
-
-# this special header allows us the detect the fallback image, if the guild does not exist or there is not emblem
-EMBLEM_STATUS_HEADER = "Emblem-Status"
 
 LOG = logging.getLogger(__name__)
 
@@ -106,7 +100,7 @@ class GuildService:
                 LOG.debug("Checks complete.")
 
                 # Icon uploading
-                icon_id, icon_content = self._download_guild_emblem(guild_id, guild_name)  # Returns None if no icon
+                icon_id, icon_content = download_guild_emblem(guild_id, guild_name)  # Returns None if no icon
                 if icon_id is not None and icon_content is not None:
                     LOG.info("Uploading icon as '%s'", icon_id)
                     ts_facade.upload_icon(icon_id, icon_content)
@@ -262,40 +256,6 @@ class GuildService:
                 f"{contacts}\n"
                 f"[hr]\n")
         return text
-
-    @staticmethod
-    def _download_guild_emblem(guild_id: str, guild_name: str, icon_size: int = 128) -> Tuple[Optional[int], Optional[bytes]]:
-        """
-        Download a guild emblem from emblem.werdes.net and checks if the response contains a real emblem or a fallback placeholder
-        :param guild_id: ID of the guild as returned from the gw2 api
-        :param guild_name: Name of the guild, mainly used for the image_iud, which is actually a hash of the guilds name
-        :param icon_size: Optional, size of the image. Anything over 128 is upscaled.
-        :return: Icon ID, Icon image data or both null
-        """
-        icon_url = f"https://emblem.werdes.net/emblem/{guild_id}/{icon_size}"
-        LOG.debug("Downloading guild emblem from %s", icon_url)
-        response = requests.get(icon_url)
-
-        if response.status_code == 200:
-            if EMBLEM_STATUS_HEADER in response.headers:
-                if response.headers[EMBLEM_STATUS_HEADER] == "OK":
-                    icon_image_data = response.content
-                    if len(icon_image_data) > 100:  # check that response actually contains some data
-                        icon_id = binascii.crc32(guild_name.encode('utf8'))
-                        return icon_id, icon_image_data
-                    else:
-                        LOG.warning("Very small Response. Guild probably has no icon or an error occured.")
-                elif response.headers[EMBLEM_STATUS_HEADER] == "NotFound":
-                    LOG.info("No emblem found for guild.")
-                else:
-                    LOG.warning("Unknown Emblem Status: %s", response.headers[EMBLEM_STATUS_HEADER])
-            else:
-                LOG.warning("%s header not found in response. Assuming this is an error.", EMBLEM_STATUS_HEADER)
-        else:
-            LOG.warning("Icon download failed, HTTP Status code was: %s", response.status_code)
-
-        # if anything failed, return None
-        return None, None
 
     def remove_guild(self, name):
         """
--- tests/test_emblem_downloader.py
+++ tests/test_emblem_downloader.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from bot import GuildService
+from bot.emblem_downloader import download_guild_emblem
 from bot.util import initialize_logging
 
 ANY_INVALID_GUILD_ID = "abc"
@@ -10,18 +10,18 @@ ANY_VALID_GUILD_ID = "14762DCE-C2A4-E711-80D5-441EA14F1E44"
 ANY_VALID_GUILD_NAME = "Lqibzzexgvkikpydotxsvijehyhexd"
 
 
-class TestGuildService(TestCase):
+class TestEmblemDownloader(TestCase):
     def setUp(self) -> None:
         super().setUp()
         initialize_logging()
 
     def test__download_guild_emblem_returns_none_on_not_existing_guild(self):
-        icon_id, icon_data = GuildService._download_guild_emblem(ANY_INVALID_GUILD_ID, ANY_INVALID_GUILD_NAME)
+        icon_id, icon_data = download_guild_emblem(ANY_INVALID_GUILD_ID, ANY_INVALID_GUILD_NAME)
         self.assertIsNone(icon_id)
         self.assertIsNone(icon_data)
 
     def test__download_guild_emblem_returns_on_existing_guild(self):
-        icon_id, icon_data = GuildService._download_guild_emblem(ANY_VALID_GUILD_ID, ANY_VALID_GUILD_NAME)
+        icon_id, icon_data = download_guild_emblem(ANY_VALID_GUILD_ID, ANY_VALID_GUILD_NAME)
         self.assertIsNotNone(icon_id)
         self.assertIsNotNone(icon_data)
         self.assertGreater(len(icon_data), 1000)
