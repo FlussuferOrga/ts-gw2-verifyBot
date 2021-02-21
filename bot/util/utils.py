52077ea8031204f969181477e851bea89f147dda
Tobias Günther <Xyaren@users.noreply.github.com>
2021-02-21 16:25:54 +0100

    fix ISE500

--- bot/util/utils.py
+++ bot/util/utils.py
@@ -3,4 +3,9 @@ import re
 
 def strip_ts_channel_name_tags(channel_name: str) -> str:
     regex = r'^\[.spacer](.+)$'
-    return re.match(regex, channel_name).group(1).strip() or channel_name.strip()
+    match = re.match(regex, channel_name)
+    if match:
+        group = match.group(1)
+        if group:
+            return group.strip()
+    return channel_name.strip()
