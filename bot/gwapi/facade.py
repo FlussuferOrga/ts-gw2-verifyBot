69a0539bf43a657e2d2460651a640c4027951c7b
Tobias Günther <Xyaren@users.noreply.github.com>
2023-03-09 09:33:32 +0100

    change logging

--- bot/gwapi/facade.py
+++ bot/gwapi/facade.py
@@ -45,9 +45,9 @@ _anonymousClient = _create_client()  # this client can be reused, to save initia
 def _check_error(result):
     if "text" in result:
         error_text = result["text"]
-        LOG.info("Api returned error: %s", error_text)
+        LOG.info("Api returned error: '%s'", error_text)
         if result.status_code == 429 or error_text == "too many requests":
-            LOG.debug("Rate Limited:" + result)
+            LOG.info("Rate Limited:" + result)
             raise ApiUnavailableError("Rate Limited:" + error_text)
         if error_text == "ErrTimeout":  # happens on login server down
             raise ApiUnavailableError(error_text)
